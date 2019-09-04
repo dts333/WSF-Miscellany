1663150b0071c2c19d04837eddfd63f6628289cf
Danny Swift <dswift333@gmail.com>
2019-09-04 15:06:12 -0400

    config

---
+++ YoutubeConfig.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Sep  4 14:10:01 2019
+
+@author: DannySwift
+"""
+
+VIDSTATS = ['average_percentage_viewed', 'views']
+VIDSTATSTITLES = ['average_watch_percentage', 'views']
+CHANNELSTATS = ['traffic_source_type']
+DATA_DIRECTORY = 'YoutubeData'
